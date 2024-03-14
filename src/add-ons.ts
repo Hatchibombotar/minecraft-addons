ba7e6b815eaf1810fa3f388058e281e6a434b336
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2024-03-14 19:09:18 +0000

    Update add-ons.ts

--- src/add-ons.ts
+++ src/add-ons.ts
@@ -12,5 +12,12 @@ export default [
         mcpedl_slug: "bundlepacks",
         github_url: "https://github.com/Hatchibombotar/bundlepacks-addon/",
         download_url: "https://github.com/Hatchibombotar/bundlepacks-addon/releases"
+    },
+    {
+        id: "fake-blocks-addon",
+        title: "Fake Blocks Add-On",
+        mcpedl_slug: "fake-blocks-addon-1",
+        github_url: "https://github.com/Hatchibombotar/fake-blocks-addon/",
+        download_url: "https://github.com/Hatchibombotar/fake-blocks-addon/releases"
     }
 ]
